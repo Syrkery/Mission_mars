b54470cc7839c857703c21467b44234e730fe9e3
Syrkery <e3075589k@gmail.com>
2025-04-15 13:02:55 +0300

    rollback

--- app.py
+++ app.py
@@ -103,22 +103,5 @@ def login():
         return render_template('login.html', title='Аварийный доступ')
 
 
-@app.route('/distribution')
-def distribution():
-    astronauts = [
-        "Ридли Скотт",
-        "Энди Уир",
-        "Марк Уотни",
-        "Венката Капур",
-        "Тедди Сандерс",
-        "Шон Бин"
-    ]
-    return render_template(
-        'distribution.html',
-        title='Размещение по каютам',
-        astronauts=astronauts
-    )
-
-
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='127.0.0.1')
